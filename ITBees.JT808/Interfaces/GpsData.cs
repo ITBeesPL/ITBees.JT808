46bfe793128d86cdf1182a819558408323ee2b0e
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-10-21 15:28:54 +0200

    VIN number support

--- ITBees.JT808/Interfaces/GpsData.cs
+++ ITBees.JT808/Interfaces/GpsData.cs
@@ -7,6 +7,7 @@ public class GpsData
 {
     public int Id { get; set; }
     public string DeviceId { get; set; }
+    public string VIN { get; set; }
     public double Latitude { get; set; }
     public double Longitude { get; set; }
     public double Speed { get; set; }
